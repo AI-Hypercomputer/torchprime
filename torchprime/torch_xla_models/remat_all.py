3505438dea72d21f18f75a56abcaa037eddd4748
Yifei Teng <yifeit@google.com>
2025-03-12 14:43:14 -0700

    Introduce two FX graph partitioners: remat_all and remat_all_and_offload_these_inputs
    The main purpose is to be able to add activation checkpointing and host
    offloading of layer inputs to scan, which will be added later.
    
    And add unit tests that they work as intended.

---
+++ torchprime/torch_xla_models/remat_all.py
@@ -0,0 +1,80 @@
+from contextlib import contextmanager
+
+import torch._functorch.config
+import torch.fx
+from functorch.compile import min_cut_rematerialization_partition
+from torch.utils.checkpoint import CheckpointPolicy
+
+
+def remat_all_partition_fn(
+  joint_module: torch.fx.GraphModule,
+  _joint_inputs,
+  *,
+  num_fwd_outputs,
+):
+  """
+  remat_all_partition_fn is a graph partition function that closely matches the
+  default behavior of `torch.utils.checkpoint`, which is to discard all intermediate
+  activations and recompute all of them during the backward pass.
+  """
+  with _remat_all_config():
+    # Mark anything that does not have a policy as MUST_RECOMPUTE
+    for node in joint_module.graph.nodes:
+      if node.op == "call_function" and "recompute" not in node.meta:
+        # This trick is taken from https://github.com/pytorch/pytorch/blob/1eba9b3aa3c43f86f4a2c807ac8e12c4a7767340/torch/utils/checkpoint.py#L1290
+        node.meta["recompute"] = CheckpointPolicy.MUST_RECOMPUTE
+
+        # min_cut_rematerialization_partition checks the graph ID to handle multiple
+        # graphs at once. We only have one graph so this can simply be 0.
+        node.meta["ac_graph_id"] = 0
+
+    return min_cut_rematerialization_partition(
+      joint_module, _joint_inputs, num_fwd_outputs=num_fwd_outputs
+    )
+
+
+@contextmanager
+def _remat_all_config():
+  # Backup existing config values
+  backup = {
+    "aggressive_recomputation": torch._functorch.config.aggressive_recomputation,
+    "recompute_views": torch._functorch.config.recompute_views,
+    "ban_recompute_reductions": torch._functorch.config.ban_recompute_reductions,
+    "ban_recompute_not_in_allowlist": torch._functorch.config.ban_recompute_not_in_allowlist,
+    "ban_recompute_materialized_backward": torch._functorch.config.ban_recompute_materialized_backward,
+    "ban_recompute_long_fusible_chains": torch._functorch.config.ban_recompute_long_fusible_chains,
+    "ban_recompute_used_far_apart": torch._functorch.config.ban_recompute_used_far_apart,
+  }
+
+  try:
+    # Don't ban the recomputing of any ops.
+    torch._functorch.config.aggressive_recomputation = True
+    torch._functorch.config.recompute_views = True
+    torch._functorch.config.ban_recompute_reductions = False
+    torch._functorch.config.ban_recompute_not_in_allowlist = False
+    torch._functorch.config.ban_recompute_materialized_backward = False
+    torch._functorch.config.ban_recompute_long_fusible_chains = False
+    torch._functorch.config.ban_recompute_used_far_apart = False
+    yield
+
+  finally:
+    # Restore the original config values
+    torch._functorch.config.aggressive_recomputation = backup[
+      "aggressive_recomputation"
+    ]
+    torch._functorch.config.recompute_views = backup["recompute_views"]
+    torch._functorch.config.ban_recompute_reductions = backup[
+      "ban_recompute_reductions"
+    ]
+    torch._functorch.config.ban_recompute_not_in_allowlist = backup[
+      "ban_recompute_not_in_allowlist"
+    ]
+    torch._functorch.config.ban_recompute_materialized_backward = backup[
+      "ban_recompute_materialized_backward"
+    ]
+    torch._functorch.config.ban_recompute_long_fusible_chains = backup[
+      "ban_recompute_long_fusible_chains"
+    ]
+    torch._functorch.config.ban_recompute_used_far_apart = backup[
+      "ban_recompute_used_far_apart"
+    ]
