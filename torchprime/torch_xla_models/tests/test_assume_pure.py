58ba60c527a338a58142c93b93b69861137293b4
Yifei Teng <yifeit@google.com>
2025-06-17 08:20:52 +0000

    Add test

--- torchprime/torch_xla_models/tests/test_assume_pure.py
+++ torchprime/torch_xla_models/tests/test_assume_pure.py
@@ -1,8 +1,12 @@
 import torch
 import torch.nn as nn
 import torch_xla
+from omegaconf import OmegaConf
 
-from torchprime.torch_xla_models.model_rewriting.assume_pure import PureModule
+from torchprime.torch_xla_models.model_rewriting.assume_pure import (
+  PureModule,
+  mark_pure_modules,
+)
 
 
 def test_nn_linear():
@@ -15,3 +19,45 @@ def test_nn_linear():
   actual_output = pure_linear(inputs)
   torch_xla.sync()
   torch.testing.assert_close(actual_output, expected_output)
+
+
+def test_rewrite():
+  # Arrange
+  class Foo(nn.Module):
+    def __init__(self):
+      super().__init__()
+
+    def forward(self, input):
+      return input
+
+  class Bar(nn.Module):
+    def __init__(self):
+      super().__init__()
+
+    def forward(self, input):
+      return input
+
+  class Model(nn.Module):
+    def __init__(self):
+      super().__init__()
+      self.foo = Foo()
+      self.bar = Bar()
+
+    def forward(self, input):
+      return self.foo(input) + self.bar(input)
+
+  model = Model()
+  config = OmegaConf.create(
+    {
+      "model": {
+        "pure_modules": ["Foo"],
+      },
+    }
+  )
+
+  # Act
+  model = mark_pure_modules(model, config)
+
+  # Assert
+  assert isinstance(model.foo, PureModule)
+  assert not isinstance(model.bar, PureModule)
--- torchprime/torch_xla_models/tests/test_trainer.py
+++ torchprime/torch_xla_models/tests/test_trainer.py
@@ -92,7 +92,6 @@ def dummy_config():
       "profile_start_step": -1,
       "profile_end_step": -1,
       "profile_dir": "/tmp/profile",
-      "profile_duration": 5,
       "ici_mesh": {"data": 1, "fsdp": 1, "tensor": 1},
       "dcn_mesh": {},
     }
