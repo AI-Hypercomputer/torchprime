d1fb7df79ea79a7a9fd6e8e3c079fc6a15ec4e1f
Jack Oh <jackoh@google.com>
2025-09-09 00:54:20 +0000

    Divide xla and torchax test in test_run_model

--- torchprime/launcher/test_run_model.py
+++ torchprime/launcher/test_run_model.py
@@ -12,22 +12,32 @@ class SimpleModel(torch.nn.Module):
     return (torch.randn(batch_size, 10),), {}
 
 
-def test_run_model():
+def test_run_model_xla():
   m = SimpleModel()
   times = run_model.run_model_xla(m, 5, 3)
   assert len(times) == 3
+
+
+def test_run_model_torchax():
+  m = SimpleModel()
   times = run_model.run_model_torchax(m, 5, 3, eager=True)
   assert len(times) == 3
   times = run_model.run_model_torchax(m, 5, 3, eager=False)
   assert len(times) == 3
 
 
-def test_run_model_transact():
+def test_run_model_transact_xla():
   model_id = "pinterest/transformer_user_action"
   model_factory = models.registry.get(model_id)
   model = model_factory()
   times = run_model.run_model_xla(model, 1, 3)
   assert len(times) == 3
+
+
+def test_run_model_transact_torchax():
+  model_id = "pinterest/transformer_user_action"
+  model_factory = models.registry.get(model_id)
+  model = model_factory()
   times = run_model.run_model_torchax(model, 1, 3, eager=True)
   assert len(times) == 3
   times = run_model.run_model_torchax(model, 1, 3, eager=False)
